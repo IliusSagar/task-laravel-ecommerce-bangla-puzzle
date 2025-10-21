643904ac5d934fefb3f892d2a4744ea415b1080f
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2025-10-21 17:14:36 +0600

    File Structure Organize & Extra Blade File Remove

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,10 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('backend.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
